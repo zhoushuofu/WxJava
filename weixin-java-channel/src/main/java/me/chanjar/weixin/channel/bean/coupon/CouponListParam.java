44331274e4062ce7d1264ada2a4c13bc7c4c652a
Binary Wang <binarywang@gmail.com>
2023-07-17 23:13:32 +0800

    :art: 格式化部分代码

--- weixin-java-channel/src/main/java/me/chanjar/weixin/channel/bean/coupon/CouponListParam.java
+++ weixin-java-channel/src/main/java/me/chanjar/weixin/channel/bean/coupon/CouponListParam.java
@@ -3,32 +3,43 @@ package me.chanjar.weixin.channel.bean.coupon;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.io.Serializable;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 /**
+ * 获取优惠券ID列表接口的请求参数
+ *
  * @author <a href="https://github.com/lixize">Zeyes</a>
  */
 @Data
 @NoArgsConstructor
 @JsonInclude(Include.NON_NULL)
 public class CouponListParam implements Serializable {
-
   private static final long serialVersionUID = 7123047113279657365L;
-  /** 优惠券状态 {@link me.chanjar.weixin.channel.enums.WxCouponStatus} */
+
+  /**
+   * 优惠券状态 {@link me.chanjar.weixin.channel.enums.WxCouponStatus}
+   */
   @JsonProperty("status")
   private Integer status;
 
-  /** 第几页（最小填1） */
+  /**
+   * 第几页（最小填1）
+   */
   @JsonProperty("page")
   private Integer page;
 
-  /** 每页数量(不超过200） */
+  /**
+   * 每页数量(不超过200）
+   */
   @JsonProperty("page_size")
   private Integer pageSize;
 
-  /** 分页上下文 */
+  /**
+   * 分页上下文
+   */
   @JsonProperty("page_ctx")
   private String pageCtx;
 }
--- weixin-java-channel/src/main/java/me/chanjar/weixin/channel/bean/coupon/UserCouponListParam.java
+++ weixin-java-channel/src/main/java/me/chanjar/weixin/channel/bean/coupon/UserCouponListParam.java
@@ -11,12 +11,14 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @NoArgsConstructor
-@EqualsAndHashCode
+@EqualsAndHashCode(callSuper = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserCouponListParam extends CouponListParam {
-
   private static final long serialVersionUID = -1056132009327357435L;
-  /** openId */
+
+  /**
+   * openId
+   */
   @JsonProperty("openid")
   private String openId;
 }
