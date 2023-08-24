7a96f0f86344f769c0feb19eca062dc677fda430
Binary Wang <binarywang@gmail.com>
2023-08-24 20:56:20 +0800

    :art: #3116【开放平台】云开发/云托管环境共享接口增加请求环境源参数

--- weixin-java-open/src/main/java/me/chanjar/weixin/open/bean/tcb/ShareCloudBaseEnvRequest.java
+++ weixin-java-open/src/main/java/me/chanjar/weixin/open/bean/tcb/ShareCloudBaseEnvRequest.java
@@ -19,11 +19,19 @@ public class ShareCloudBaseEnvRequest implements Serializable {
   @SerializedName("data")
   private List<DataDTO> data;
 
+  /**
+   * 请求环境源，填 1，表示云托管环境
+   */
+  @SerializedName("source_type")
+  private Integer sourceType;
+
   @Data
   @Builder
   @AllArgsConstructor
   @NoArgsConstructor
   public static class DataDTO implements Serializable {
+    private static final long serialVersionUID = -8117487215582856716L;
+
     @SerializedName("env")
     private String env;
     @SerializedName("appids")
