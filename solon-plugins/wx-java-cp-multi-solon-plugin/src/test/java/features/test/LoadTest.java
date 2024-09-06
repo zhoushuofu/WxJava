a6e3c865052988ab41e4cfd701e6ccde2d5c839e
西东 <noear@foxmail.com>
2024-09-06 23:46:08 +0800

    :art: solon-plugins 添加单元测试支持，可触发必要错误用于人工核对

---
+++ solon-plugins/wx-java-cp-multi-solon-plugin/src/test/java/features/test/LoadTest.java
@@ -0,0 +1,15 @@
+package features.test;
+
+import org.junit.jupiter.api.Test;
+import org.noear.solon.test.SolonTest;
+
+/**
+ * @author noear 2024/9/4 created
+ */
+@SolonTest
+public class LoadTest {
+  @Test
+  public void load(){
+
+  }
+}
